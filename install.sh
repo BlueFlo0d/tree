a970ddec5026a1294120548eadf09b069494e129
Qiantan Hong <qhong@mit.edu>
2020-04-09 16:35:24 -0400

    install scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-brew install clozure-cl
 brew cask install supercollider
-brew install wget
-brew install sdl2
-brew install sdl2_ttf
-brew install sdl2_image
+brew uninstall clozure-cl wget sdl2 sdl2_ttf sdl2_image
+brew install clozure-cl wget sdl2 sdl2_ttf sdl2_image
+rm -rf ~/quicklisp
 wget https://beta.quicklisp.org/quicklisp.lisp
 ccl64 --load ./quicklisp.lisp \
       --eval '(quicklisp-quickstart:install)'       \
       --eval '(ql:add-to-init-file)'                \
       --eval '(quit)'
-
+wget https://common-lisp.net/project/asdf/archives/asdf.lisp
+cp asdf.lisp `brew list clozure-cl | grep asdf.lisp`
 exec ./run.sh
