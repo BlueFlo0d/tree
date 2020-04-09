f26f591403d7c540e1c8020986c647c0522c0b4b
Qiantan Hong <qhong@mit.edu>
2020-04-09 15:23:12 -0400

    dependency

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 brew install clozure-cl
 brew cask install supercollider
 brew install wget
+brew install sdl2
+brew install sdl2_ttf
+brew install sdl2_image
 wget https://beta.quicklisp.org/quicklisp.lisp
 ccl64 --load ./quicklisp.lisp \
       --eval '(quicklisp-quickstart:install)'       \
